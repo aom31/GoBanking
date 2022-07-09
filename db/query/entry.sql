0aeb8d26216d3bfd9ebd9e04fce5ed4ebef492d0
thamakorn <97218117+aom31@users.noreply.github.com>
2022-07-09 19:32:19 +0700

    sqlc gen update

--- db/query/entry.sql
+++ db/query/entry.sql
@@ -1,7 +1,7 @@
 -- name: CreateEntries :one
 INSERT INTO entries (
   account_id,
-  amount,
+  amount
 ) VALUES (
   $1, $2
 ) RETURNING *;
@@ -10,7 +10,7 @@ INSERT INTO entries (
 SELECT * FROM entries
 WHERE id = $1 LIMIT 1;
 
--- name: ListAccounts :many
+-- name: ListEntries :many
 SELECT * FROM entries
 WHERE account_id = $1
 ORDER BY id
