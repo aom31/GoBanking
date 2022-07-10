789289a8a920f9ce054430b06dcdd746f538d752
aom31 <97218117+aom31@users.noreply.github.com>
2022-07-10 18:09:53 +0700

    Update account_test.go

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -3,13 +3,14 @@ package db
 import (
 	"context"
 	"example/gobanking/util"
+	"time"
 
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
 
-func TestCreateAccount(t *testing.T) {
+func createRandomAccount(t *testing.T) Account {
 	arg := CreateAccountParams{
 		Owner:    util.RandomOwner(),
 		Balance:  util.RandomMoney(),
@@ -26,4 +27,46 @@ func TestCreateAccount(t *testing.T) {
 	require.NotZero(t, account.ID)
 	require.NotZero(t, account.CreatedAt)
 
+	return account
+}
+
+func TestCreateAccount(t *testing.T) {
+	createRandomAccount(t)
+}
+
+func TestGetAccount(t *testing.T) {
+	account1 := createRandomAccount(t)
+	account2, err := testQueries.GetAccount(context.Background(), account1.ID)
+	require.NoError(t, err) //check error should be nil
+	require.NotEmpty(t, account2)
+
+	require.Equal(t, account1.ID, account2.ID) //map  acc2 should equal acc1 all field
+	require.Equal(t, account1.Owner, account2.Owner)
+	require.Equal(t, account1.Balance, account2.Balance)
+	require.Equal(t, account1.Currency, account2.Currency)
+	require.WithinDuration(t, account1.CreatedAt, account2.CreatedAt, time.Second)
+}
+
+func TestUpdateAccount(t *testing.T) {
+	//create new account1
+	account1 := createRandomAccount(t)
+
+	//declare arguments
+	arg := UpdateAccountParams{
+		ID:      account1.ID,
+		Balance: util.RandomMoney(),
+	}
+
+	//call test query
+	account2, err := testQueries.UpdateAccount(context.Background(), arg) //pass arg for update argument
+	require.NoError(t, err)
+	require.NotEmpty(t, account2) //should not be empty
+
+	//and compare each individaul field of acc2 to acc1
+	require.Equal(t, account1.ID, account2.ID)
+	require.Equal(t, account1.Owner, account2.Owner)
+	require.Equal(t, arg.Balance, account2.Balance)
+	require.Equal(t, account1.Currency, account2.Currency)
+
+	require.WithinDuration(t, account1.CreatedAt, account2.CreatedAt, time.Second)
 }
