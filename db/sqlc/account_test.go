7f2272094ba28b12f0d4fca50d8dba42df55e178
aom31 <97218117+aom31@users.noreply.github.com>
2022-07-10 17:13:45 +0700

    update testfile

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -2,6 +2,8 @@ package db
 
 import (
 	"context"
+	"example/gobanking/util"
+
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -9,9 +11,9 @@ import (
 
 func TestCreateAccount(t *testing.T) {
 	arg := CreateAccountParams{
-		Owner:    "aom",
-		Balance:  500,
-		Currency: "USD",
+		Owner:    util.RandomOwner(),
+		Balance:  util.RandomMoney(),
+		Currency: util.RandomCurrency(),
 	}
 	account, err := testQueries.CreateAccount(context.Background(), arg)
 	require.NoError(t, err)
